904ef5efbd10e19982a572e35b7df0e5868ba3d3
movy18x <movy18x@gmail.com>
2022-09-13 02:03:00 +0300

    fix some bugs

--- Nodejs Pass/fs.js
+++ Nodejs Pass/fs.js
@@ -1,8 +1,6 @@
 import fs from "fs";
-export var alice = fs
-  .readFileSync("./orders/aliceOreder.txt", "utf-8")
-  .toString();
-export var mery = fs.readFileSync("./orders/meryOrder.txt", "utf-8").toString();
-export var mohammed = fs
-  .readFileSync("./orders/mohammedOrder.txt", "utf-8")
-  .toString();
+let data = fs.readFileSync("./order.txt", "utf8").toString().split(",");
+
+export let alice = data[0].toString();
+export let mery = data[1].toString();
+export let mohammed = data[2].toString();
--- Nodejs Pass/orderSchema.js
+++ Nodejs Pass/orderSchema.js
@@ -4,44 +4,47 @@ let finalTotal = 0;
 let finalSchema = [];
 
 orders.forEach((order) => {
-  let a = order.split(",");
-  a.map((x) => {
-    if (x != "") {
-      let oneOrder = x.split("\r\n");
-      let items = Number(oneOrder[2]);
-      let info = oneOrder[3 + items].split(" ");
-
-      let name = oneOrder[0];
-      let mainaddress = oneOrder[1].split(" ");
-      let array = [];
-      let mainTotal = 0;
-      for (let index = 0; index < items; index++) {
-        let item = oneOrder[index + 3].split(" ");
-
-        let itemSchema = {
-          name: item[0],
-          count: item[1],
-          price: item[2],
-          total: item[3],
-        };
-        mainTotal += Number(item[3]);
-        array.push(itemSchema);
+  order = order.split("\r\n");
+  order.forEach((row) => {
+    if (row == "") {
+      let idx = order.indexOf(row);
+      if (idx > -1) {
+        order.splice(idx, 1); // delete empty rows
       }
-      finalTotal += mainTotal;
-
-      let schema = {
-        customer: name,
-        address: {
-          latitude: mainaddress[0],
-          longitude: mainaddress[1],
-        },
-        items: array,
-        total: mainTotal,
-        discount: Number(info[1]),
-        totalAfterDiscount: mainTotal - mainTotal * (Number(info[1]) / 100),
-      };
-      finalSchema.push(schema);
     }
   });
+  let name = order[0];
+  let mainAddress = order[1].split(" ");
+  let items = Number(order[2]);
+  let info = order[3 + items].toString().split(" ");
+
+  let array = [];
+  let mainTotal = 0;
+
+  for (let index = 0; index < items; index++) {
+    let oneItem = order[index + 3].split(" ");
+    let itemSchema = {
+      name: oneItem[0],
+      count: oneItem[1],
+      price: oneItem[2],
+      total: oneItem[3],
+    };
+    mainTotal += Number(oneItem[3]);
+    array.push(itemSchema);
+  }
+  finalTotal += mainTotal;
+
+  let schema = {
+    customer: name,
+    address: {
+      latitude: mainAddress[0],
+      longitude: mainAddress[1],
+    },
+    items: array,
+    total: mainTotal,
+    discount: Number(info[1]),
+    totalAfterDiscount: mainTotal - mainTotal * (Number(info[1]) / 100),
+  };
+  finalSchema.push(schema);
 });
 export { finalSchema, finalTotal };
